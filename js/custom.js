ec8399836e29e10efcf42981327a908a799717eb
raetast <raetast@gmail.com>
2025-11-29 01:05:49 +0900

    new site layout

--- js/custom.js
+++ js/custom.js
@@ -22,35 +22,6 @@
     });
 
 
-<<<<<<< HEAD
-    // work
-
-    $('.owl-work').owlCarousel({
-      animateOut: 'fadeOut',
-      loop: true,
-      autoplayHoverPause: false,
-      autoplay: true,
-      smartSpeed: 1000,
-      dots: false,
-      nav:true,
-      navText: [
-          '<i class="fa fa-angle-left"></i>',
-          '<i class="fa fa-angle-right"></i>'
-      ],
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        1000: {
-          items: 3,
-        }
-      }
-    });
-
-
-=======
->>>>>>> 55b91b2 (renewed)
     // SMOOTHSCROLL
     $(function() {
       $('.custom-navbar a, #home a').on('click', function(event) {
